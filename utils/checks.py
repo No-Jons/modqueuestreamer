d367d6af48de6f8420ae1fe1f29cb94705afae93
No-Jons <61525221+No-Jons@users.noreply.github.com>
2020-10-19 21:32:06 -0700

    Debugged get_subreddit

--- utils/checks.py
+++ utils/checks.py
@@ -9,7 +9,6 @@ class Checks:
 
     async def get_subreddit(self, ctx, subreddit):
         subs = list()
-        # todo: multi-page subreddit choice for multi-sub guilds?
         for item in self.bot.channel_config.keys():  # holy shit this code sucks
             if self.bot.channel_config[item]["guild"] == ctx.guild.id:
                 if item == subreddit.lower():
@@ -20,10 +19,11 @@ class Checks:
                 await ctx.send("Your subreddit has not been verified yet! Fix that by using the `r!register` command!")
                 self.bot.logger.info("Verification failed: no subreddit provided")
                 return None
+            if len(subs) == 1:
+                return subs[0]
             if len(subs) > 1:
                 if len(subs) > 9:
                     subs = subs[:9]
-                # todo: selection embed
                 embed = discord.Embed(title="Choose a subreddit to edit the config of:", color=discord.Color.green())
                 for i in range(len(subs)):
                     embed.add_field(name=f"{self._emojis[i]}.", value=f"/r/{subs[i]}")
@@ -31,7 +31,7 @@ class Checks:
                 for i in range(len(subs)):
                     await message.add_reaction(self._emojis[i])
                 r, u = await self.bot.wait_for('reaction_add',
-                                          check= lambda r, u: r.emoji in self._emojis and u.id == ctx.author.id)
+                                               check= lambda r, u: r.emoji in self._emojis and u.id == ctx.author.id)
                 await message.delete()
                 list_index = self._emojis.index(r.emoji)
                 return subs[list_index]
