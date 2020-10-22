e45f6dce448d16a4e1bfbe98bea6fb0f699a463f
No-Jons <61525221+No-Jons@users.noreply.github.com>
2020-10-22 01:39:02 -0700

    Added report count to item embed

--- utils/formatter.py
+++ utils/formatter.py
@@ -30,15 +30,17 @@ def format_msg(obj, approved=False, removed=False):
     author_name = str(obj.author) if obj.author else "[deleted]"
     embed.add_field(name="Author", value=f"[u/{author_name}](https://www.reddit.com/user/{author_name})")
     if obj.user_reports:
-        reports = str()
-        for i in obj.user_reports:
-            reports += f"{i[1]}: {i[0]}\n"
-        embed.add_field(name="User Reports:", value=reports)
+        user_reports = obj.user_reports
+        if len(user_reports) >= 5:
+            user_reports = obj.user_reports[:5]
+        embed.add_field(name=f"User Reports ({len(obj.user_reports)}):",
+                        value="".join([f"{i[1]}: {i[0]}\n" for i in user_reports]))
     if obj.mod_reports:
-        reports = str()
-        for i in obj.mod_reports:
-            reports += f"{i[1]}: {i[0]}\n"
-        embed.add_field(name="Mod Reports:", value=reports)
+        mod_reports = obj.mod_reports
+        if len(mod_reports) >= 5:
+            mod_reports = obj.mod_reports[:5]
+        embed.add_field(name=f"Mod Reports ({len(obj.mod_reports)}):",
+                        value="".join([f"{i[1]}: {i[0]}\n" for i in mod_reports]))
     embed.set_footer(text=f"/r/{obj.subreddit} - {obj.fullname} {footer_suffix}")
     embed.timestamp = datetime.fromtimestamp(obj.created_utc)
     return embed
